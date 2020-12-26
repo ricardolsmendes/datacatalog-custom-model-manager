30c88a56768c2c20a0f513af3d2659ab2b3427f6
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-12-26 19:29:40 -0300

    Simplify the CLI test methods names

--- tests/datacatalog_custom_model_manager/custom_model_manager_cli_test.py
+++ tests/datacatalog_custom_model_manager/custom_model_manager_cli_test.py
@@ -9,18 +9,14 @@ class CustomModelManagerCLITest(unittest.TestCase):
     __CLI_MODULE = 'datacatalog_custom_model_manager.custom_model_manager_cli'
     __CLI_CLASS = f'{__CLI_MODULE}.CustomModelManagerCLI'
 
-    @mock.patch(f'{__CLI_CLASS}._CustomModelManagerCLI__load_custom_model')
     @mock.patch(f'{__CLI_CLASS}._parse_args')
-    def test_run_load_model_should_parse_args(self, mock_parse_args, mock_load_custom_model):
-        mock_parse_args.return_value.func = mock_load_custom_model
+    def test_run_should_parse_args(self, mock_parse_args):
         custom_model_manager_cli.CustomModelManagerCLI.run([])
         mock_parse_args.assert_called_once()
 
     @mock.patch(f'{__CLI_CLASS}._CustomModelManagerCLI__load_custom_model')
     @mock.patch(f'{__CLI_CLASS}._parse_args')
-    def test_run_load_model_should_call_load_custom_model(self, mock_parse_args,
-                                                          mock_load_custom_model):
-
+    def test_run_load_should_call_load_custom_model(self, mock_parse_args, mock_load_custom_model):
         mock_parse_args.return_value.func = mock_load_custom_model
         custom_model_manager_cli.CustomModelManagerCLI.run([])
         mock_load_custom_model.assert_called_once_with(mock_parse_args.return_value)
@@ -29,11 +25,11 @@ class CustomModelManagerCLITest(unittest.TestCase):
         self.assertRaises(SystemExit, custom_model_manager_cli.CustomModelManagerCLI._parse_args,
                           ['create'])
 
-    def test_parse_args_load_model_missing_mandatory_args_should_raise_system_exit(self):
+    def test_parse_args_load_missing_mandatory_args_should_raise_system_exit(self):
         self.assertRaises(SystemExit, custom_model_manager_cli.CustomModelManagerCLI._parse_args,
                           ['load'])
 
-    def test_parse_args_load_model_should_parse_mandatory_args(self):
+    def test_parse_args_load_should_parse_mandatory_args(self):
         args = custom_model_manager_cli.CustomModelManagerCLI._parse_args([
             'load', '--files-folder', 'test-folder', '--project-id', 'test-project',
             '--location-id', 'test-location'
@@ -42,7 +38,7 @@ class CustomModelManagerCLITest(unittest.TestCase):
         self.assertEqual('test-project', args.project_id)
         self.assertEqual('test-location', args.location_id)
 
-    def test_parse_args_load_model_should_parse_optional_args(self):
+    def test_parse_args_load_should_parse_optional_args(self):
         args = custom_model_manager_cli.CustomModelManagerCLI._parse_args([
             'load', '--files-folder', 'test-folder', '--project-id', 'test-project',
             '--location-id', 'test-location', '--delete-existing-tag-templates'
@@ -50,7 +46,7 @@ class CustomModelManagerCLITest(unittest.TestCase):
         self.assertTrue(args.delete_existing_tag_templates)
 
     @mock.patch(f'{__CLI_CLASS}._CustomModelManagerCLI__load_custom_model')
-    def test_parse_args_load_model_should_set_default_function(self, mock_load_custom_model):
+    def test_parse_args_load_should_set_default_function(self, mock_load_custom_model):
         args = custom_model_manager_cli.CustomModelManagerCLI._parse_args([
             'load', '--files-folder', 'test-folder', '--project-id', 'test-project',
             '--location-id', 'test-location'
