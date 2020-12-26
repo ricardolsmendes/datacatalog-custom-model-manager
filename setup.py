fa519fddab68c96f5c3e079726d142365f50b149
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-12-26 19:29:54 -0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='datacatalog-custom-model-manager',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/ricardolsmendes/datacatalog-custom-model-manager',
     author='Ricardo Mendes',
     author_email='ricardolsmendes@gmail.com',
